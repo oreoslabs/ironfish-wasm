b85e3f824238c4ad0fd22108db70b0bbccdd4401
louiswt <liuwentong@web3.com>
2023-12-13 21:31:40 +0800

    use vite

--- ironfish_wasm/src/lib.rs
+++ ironfish_wasm/src/lib.rs
@@ -16,20 +16,6 @@ pub mod wasm_structs;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn hello_world() {
-    console::log_1(&JsValue::from_str("Hello World!"));
-}
-
-#[wasm_bindgen]
-pub fn fib(n: i32) -> i32 {
-    match n {
-        0 => 0,
-        1 => 1,
-        _ => fib(n - 1) + fib(n - 2),
-    }
-}
-
-#[wasm_bindgen]
 pub fn send_array_to_js() -> Box<[JsValue]> {
     vec![
         JsValue::NULL,
@@ -64,37 +50,6 @@ pub fn send_obj_to_js() -> JsValue {
     };
 
     serde_wasm_bindgen::to_value(&obj).unwrap()
-}
-
-#[wasm_bindgen(module = "/js2rust/point.js")]
-extern "C" {
-    pub type Point;
-
-    #[wasm_bindgen(constructor)]
-    fn new(x: i32, y: i32) -> Point;
-
-    #[wasm_bindgen(method, getter)]
-    fn get_x(this: &Point) -> i32;
-
-    #[wasm_bindgen(method, getter)]
-    fn get_y(this: &Point) -> i32;
-
-    #[wasm_bindgen(method, setter)] //5
-    fn set_x(this: &Point, x: i32) -> i32;
-
-    #[wasm_bindgen(method, setter)]
-    fn set_y(this: &Point, y: i32) -> i32;
-
-    #[wasm_bindgen(method)]
-    fn add(this: &Point, p: Point);
-}
-
-#[wasm_bindgen]
-pub fn test_point() -> Point {
-    let p = Point::new(10, 10);
-    let p1 = Point::new(6, 3);
-    p.add(p1);
-    p
 }
 
 #[wasm_bindgen]
